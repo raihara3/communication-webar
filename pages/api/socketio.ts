6fba8fe4916a8553c347547a4dbde8ef9ba80c9a
raihara3 <raihara3@gmail.com>
2020-12-29 22:45:56 +0900

    :construction: add websocket api

---
+++ pages/api/socketio.ts
@@ -0,0 +1,33 @@
+import { Server } from 'socket.io'
+
+const ioHandler = (req, res) => {
+  if (!res.socket.server.io) {
+    console.log('*First use, starting socket.io')
+
+    const io = new Server(res.socket.server)
+    io.on('connection', socket => {
+
+      socket.on('add user', () => {
+        socket.broadcast.emit('add user', 'hoge')
+      })
+
+      socket.on('send data', data => {
+        console.log(data)
+        socket.emit('get data', data)
+      })
+    })
+
+    res.socket.server.io = io
+  } else {
+    console.log('socket.io already running')
+  }
+  res.end()
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default ioHandler
--- pages/remote.tsx
+++ pages/remote.tsx
@@ -1,6 +1,43 @@
+import { useEffect } from "react"
+import io from 'socket.io-client'
+
 const Remote = () => {
+  const connectWebsocket = async() => {
+    await fetch('api/socketio')
+    const socket = io()
+
+    socket.on('connect', () => {
+      socket.emit('add user')
+      console.log(`join`)
+    })
+
+    socket.on('disconnect', () => {
+      console.log('disconnect')
+    })
+
+    socket.on('add user', data => {
+      console.log(`add user: ${data}`)
+    })
+
+    socket.on('get data', data => {
+      console.log('get data', data)
+    })
+
+    socket.emit('send data', ({
+      hoge: 'hoge',
+      fuga: 'fuga',
+      num: [1,2,3]
+    }))
+  }
+
+  useEffect(() => {
+    connectWebsocket()
+  }, [])
+
   return (
-    <div>remote</div>
+    <>
+      <div>remote</div>
+    </>
   )
 }
 
