b7cc733c0ac629592a61b669af14db972fd4e976
raihara3 <raihara3@gmail.com>
2020-12-30 11:50:36 +0900

    :heavy_plus_sign: add material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,18 @@
 import Link from 'next/link'
-import styled from 'styled-components'
-
-const Button = styled.button`
-  border: solid 1px black;
-
-  &:hover {
-    background-color: black;
-    color: white;
-  }
-`
-
-const DisabledButton = styled(Button)`
-  opacity: 0.4;
-  pointer-events: none;
-`
-
-const Atag = styled.a`
-  display: inline-block;
-  padding: 20px;
-`
+import { ButtonGroup, Button } from '@material-ui/core';
 
 const Index = ()  => {
   return (
     <div>
       <h1>Pocket Space</h1>
-      <DisabledButton>
-        <Link href='/local'>
-          <Atag>local mode</Atag>
-        </Link>
-      </DisabledButton>
-      <Button>
-        <Link href='/remote'>
-          <Atag>remote mode</Atag>
-        </Link>
-      </Button>
+      <ButtonGroup color="primary" aria-label="outlined primary button group">
+        <Button disabled>
+          <Link href='/local'>LOCAL MODE</Link>
+        </Button>
+        <Button>
+          <Link href='/remote'>REMOTE MODE</Link>
+        </Button>
+      </ButtonGroup>
     </div>
   )
 }
