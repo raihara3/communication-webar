8f37960d580418f1dff4052839491adf56043437
raihara3 <raihara3@gmail.com>
2021-02-22 00:29:30 +0900

    :package: update the three.js package

--- pages/api/createRoom.ts
+++ pages/api/createRoom.ts
@@ -1,11 +1,8 @@
-import redis from 'redis'
 import RoomRepository from '../../core/repository/room/RoomRepository'
 import createRoomService from '../../core/service/room/CreateRoomService'
 
 const createRoomHandler = async(_, res) => {
-  const client = redis.createClient()
-
-  const roomRepository = new RoomRepository(client)
+  const roomRepository = new RoomRepository()
   const roomID = new createRoomService(roomRepository).execute()
 
   res.status(200).json({roomID: roomID})
