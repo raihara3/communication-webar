584ca22ce33bcf00dd31d7f2d9a9dbc5e76af99e
raihara3 <raihara3@gmail.com>
2021-02-22 16:46:25 +0900

    add list method to MemberRepository

--- core/repository/user/MemberRepository.ts
+++ core/repository/user/MemberRepository.ts
@@ -13,6 +13,18 @@ class MemberRepository {
     this.inner.expire(roomID, 60 * 60 * 24 * 3)
   }
 
+  list(roomID: string): Promise<Array<string>> {
+    return new Promise((resolve, reject) => {
+      this.inner.lrange(roomID, 0, -1, (error, reply) => {
+        if(error) {
+          reject(error)
+          return
+        }
+        resolve(reply)
+      })
+    })
+  }
+
   remove(roomID: string, userID: string) {
     this.inner.lrem(roomID, 0, userID)
   }
