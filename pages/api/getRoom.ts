3121674105be9386ad305c8f3828f27d9c411fd8
raihara3 <raihara3@gmail.com>
2021-02-24 12:53:00 +0900

    Change the api/getRoom.ts

--- pages/api/getRoom.ts
+++ pages/api/getRoom.ts
@@ -5,9 +5,7 @@ import GetRoomService from '../../core/service/room/GetRoomService'
 
 const getRoomHandler = async(req, res) => {
   const params: any = getUrlParams(req.headers.referer)
-  const roomID = params?.room
-    ? params?.room
-    : ''
+  const roomID = params?.room ? params?.room : ''
   if(!roomID) {
     res.status(400).json({message: 'The roomID is not specified.'})
     res.end()
@@ -15,17 +13,30 @@ const getRoomHandler = async(req, res) => {
   }
 
   const roomStorage = redis.createClient({db: 0})
-  const roomRepository = new RoomRepository(roomStorage)
-  const { hasRoom, remainingTime } = await new GetRoomService(roomRepository).execute(roomID)
-  if(!hasRoom) {
-    console.log('404')
-    res.status(404).json({message: 'This RoomID does not exist.'})
-    res.end()
-    return
-  }
 
-  res.status(200).json({data: {remainingTime: remainingTime}})
-  res.end()
+  const timeout = setTimeout(() => {
+    roomStorage.quit()
+    res.status(500).json({message: 'Database connection error'})
+    res.end()
+  }, 2000)
+  timeout
+
+  roomStorage.on('connect', async() => {
+    clearTimeout(timeout)
+    const roomRepository = new RoomRepository(roomStorage)
+    const { hasRoom, remainingTime } = await new GetRoomService(roomRepository).execute(roomID)
+    roomStorage.quit()
+    if(!hasRoom) {
+      res.status(404).json({message: 'This RoomID does not exist.'})
+      res.end()
+      return
+    }
+    res.status(200).json({data: {remainingTime: remainingTime}})
+    res.end()
+  })
+
+  roomStorage.on('error', (e) => console.log(e))
+
 }
 
 export const config = {
