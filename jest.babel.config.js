7cc946edaf5b3fee4cc96bc1fc2cc53bec7334cc
raihara3 <raihara3@gmail.com>
2021-03-01 18:54:39 +0900

    :package: add react-test-renderer

--- jest.babel.config.js
+++ jest.babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
       '@babel/preset-env',
       {targets: {node: 'current'}}
     ],
+    '@babel/preset-react',
     '@babel/preset-typescript',
   ],
   transform: {"\\.ts$": ['ts-jest']}
